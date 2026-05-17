47f3db0cf94d52047a1ffa0644ba317d7f2d0164
Tomas <tomasburack@gmail.com>
2026-05-16 21:40:17 -0300

    New Migration, Veterinarian entity/table changed, deleted idUser

--- src/VetCareBackend.Domain/Entities/Veterinarian.cs
+++ src/VetCareBackend.Domain/Entities/Veterinarian.cs
@@ -7,7 +7,6 @@ namespace VetCareBackend.Domain.Entities
     public class Veterinarian : User
     {
         public string Enrollment { get; set; } = string.Empty;
-        public Guid UserId { get; set; }
-        public User? User { get; set; }
+        
     }
 }
---
+++ src/VetCareBackend.Infrastructure/Migrations/20260517003533_DeletedUserIdInVeterinarianTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VetCareBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeletedUserIdInVeterinarianTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Veterinarians_UserId",
+                table: "Veterinarians");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Veterinarians");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Veterinarians",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veterinarians_UserId",
+                table: "Veterinarians",
+                column: "UserId");
+        }
+    }
+}
