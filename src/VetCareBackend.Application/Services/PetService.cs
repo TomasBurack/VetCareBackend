3c1e69d440995655c34c71ad3b5885777cd56818
Agustin Sentis <agussentis@gmail.com>
2026-05-19 22:24:38 -0300

    Added: Service Folder and PetService

---
+++ src/VetCareBackend.Application/Services/PetService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VetCareBackend.Application.dtos.Requests;
+using VetCareBackend.Application.dtos.Responses;
+using VetCareBackend.Application.Interfaces;
+
+namespace VetCareBackend.Application.Services
+{
+    public class PetService : IPetService
+    {
+        public PetResponse Create(PetRequest petReq)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PetResponse> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PetResponse GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(PetRequest petReq, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
