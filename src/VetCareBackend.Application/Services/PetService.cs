b9d9bfc9d9ad5e3eff18d84d9501e8f8faa77de3
Agustin-Sentis <agussentis@gmail.com>
2026-05-26 21:47:02 -0300

    Added: PetController and PetService logic.

--- src/VetCareBackend.Application/Services/PetService.cs
+++ src/VetCareBackend.Application/Services/PetService.cs
@@ -3,37 +3,77 @@ using System.Collections.Generic;
 using System.Text;
 using VetCareBackend.Application.dtos.Requests;
 using VetCareBackend.Application.dtos.Responses;
+using VetCareBackend.Application.Exceptions;
+using VetCareBackend.Application.Infrastructure;
 using VetCareBackend.Application.Interfaces;
+using VetCareBackend.Application.Mapper;
+using VetCareBackend.Domain.Entities;
 
 namespace VetCareBackend.Application.Services
 {
     public class PetService : IPetService
     {
-        
+        private readonly IPetRepository _petRepository;
+
+        public PetService(IPetRepository petRepo)
+        {
+            _petRepository = petRepo;
+        }
 
         public PetResponse Create(PetRequest petReq)
         {
-            throw new NotImplementedException();
+            var newPet = petReq.ToPet();
+            _petRepository.Add(newPet);
+            return newPet.ToPetResponse();
         }
 
-        public void Delete(PetRequest petReq, Guid id)
+        public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            var pet = _petRepository.Get(id);
+
+            if (pet == null) 
+            {
+                throw new NotFoundException($"No se encontro ninguna mascota con id '{id}'.");
+            }
+
+            _petRepository.Delete(id);
         }
 
         public List<PetResponse> GetAll()
         {
-            throw new NotImplementedException();
+            return _petRepository
+                .GetAll()
+                .Select(p => p.ToPetResponse())
+                .ToList();
         }
 
         public PetResponse GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var pet = _petRepository.Get(id);
+
+            if (pet == null)
+            {
+                throw new NotFoundException($"No se encontro ninguna mascota con id '{id}'.");
+            }
+
+            return pet.ToPetResponse();
         }
 
-        void IPetService.Update(PetRequest petReq, Guid id)
+        public void Update(PetRequest petReq, Guid id)
         {
-            throw new NotImplementedException();
+            var petToUpdate = _petRepository.Get(id);
+
+            if (petToUpdate == null)
+            {
+                throw new NotFoundException($"No se encontro ninguna mascota con id '{id}'.");
+            }
+
+            petToUpdate.Name = petReq.Name;
+            petToUpdate.Age = petReq.Age;
+            petToUpdate.TypePet = petReq.typePet;
+            petToUpdate.Breed = petReq.Breed;
+
+            _petRepository.Update(petToUpdate);
         }
     }
 }
---
+++ src/VetCareBackend/Controllers/PetController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VetCareBackend.Application.dtos.Requests;
+using VetCareBackend.Application.dtos.Responses;
+using VetCareBackend.Application.Exceptions;
+using VetCareBackend.Application.Interfaces;
+
+namespace VetCareBackend.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : Controller
+    {
+        private readonly IPetService _petService;
+
+        public PetController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<PetResponse>>  GetAll()
+        {
+            try
+            {
+                var pets = _petService.GetAll();
+
+                if (!pets.Any())
+                    return NotFound("No ha mascotas registradas.");
+
+                return Ok(pets);
+            }
+            catch (DatabaseException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("({id")]
+        public ActionResult<PetResponse> GetById([FromRoute]Guid id)
+        {
+            try
+            {
+                return Ok(_petService.GetById(id));
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (DatabaseException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrio un error inesperado.");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<PetResponse> Create([FromBody] PetRequest pet)
+        {
+            try
+            {
+                var createdPet = _petService.Create(pet);
+                return CreatedAtAction(nameof(GetById), new { id = createdPet.IdPet }, createdPet);
+            }
+            catch (DatabaseException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrió un error inesperado.");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] Guid id) 
+        {
+            try
+            {
+                _petService.Delete(id);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (DatabaseException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrió un error inesperado.");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update([FromBody] PetRequest pet, [FromRoute] Guid id)
+        {
+            try
+            {
+                _petService.Update(pet, id);
+                return NoContent();
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (DatabaseException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Ocurrió un error inesperado.");
+            }
+        }
+    }
+}
