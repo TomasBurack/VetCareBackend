0095456fbdec303b16ae2a3eac8b94d4bfb44dee
Tomas <tomasburack@gmail.com>
2026-06-04 00:29:36 -0300

    Added custom messages for validations

--- src/VetCareBackend.Application/Validations/SignUpValidation.cs
+++ src/VetCareBackend.Application/Validations/SignUpValidation.cs
@@ -11,12 +11,27 @@ namespace VetCareBackend.Application.Validations
     {
         public SignUpValidator()
         {
-            RuleFor(ur => ur.FirstName).NotEmpty().MinimumLength(3).MaximumLength(15);
-            RuleFor(ur => ur.LastName).NotEmpty().MinimumLength(3).MaximumLength(15);
-            RuleFor(ur => ur.Dni).NotEmpty().Length(8);
-            RuleFor(ur => ur.Email).NotEmpty().EmailAddress();
-            RuleFor(ur => ur.Password).NotEmpty().MinimumLength(10);
-            RuleFor(ur => ur.PhoneNumber).MinimumLength(9).MaximumLength(11);
+            RuleFor(ur => ur.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MinimumLength(3).WithMessage("First name must be at least 3 characters long.")
+                .MaximumLength(15).WithMessage("First name cannot exceed 15 characters.");
+            RuleFor(ur => ur.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MinimumLength(3).WithMessage("Last name must be at least 3 characters long.")
+                .MaximumLength(15).WithMessage("Last name cannot exceed 15 characters.");
+            RuleFor(ur => ur.Dni)
+                .NotEmpty().WithMessage("DNI is required.")
+                .Length(8).WithMessage("DNI must be exactly 8 characters long.");
+            RuleFor(ur => ur.Email)
+                .NotEmpty().WithMessage("Email address is required.")
+                .EmailAddress().WithMessage("The email address format is not valid.");
+            RuleFor(ur => ur.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(10).WithMessage("Password must be at least 10 characters long.");
+            RuleFor(ur => ur.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .MinimumLength(9).WithMessage("Phone number must be at least 9 characters long.")
+                .MaximumLength(11).WithMessage("Phone number cannot exceed 11 characters.");
         }
     }
 }
--- src/VetCareBackend.Application/Validations/UserRequestValidation.cs
+++ src/VetCareBackend.Application/Validations/UserRequestValidation.cs
@@ -10,11 +10,24 @@ namespace VetCareBackend.Application.Validations
     {
         public UserRequestValidation()
         {
-            RuleFor(request => request.FirstName).MinimumLength(3).MaximumLength(15).When(request => !string.IsNullOrWhiteSpace(request.FirstName));
-            RuleFor(request => request.LastName).MinimumLength(3).MaximumLength(15).When(request => !string.IsNullOrWhiteSpace(request.LastName));
-            RuleFor(request => request.Dni).Length(8).When(request => !string.IsNullOrWhiteSpace(request.Dni));
-            RuleFor(request => request.Email).EmailAddress().When(request => !string.IsNullOrWhiteSpace(request.Email));
-            RuleFor(request => request.PhoneNumber).MinimumLength(9).MaximumLength(11).When(request => !string.IsNullOrWhiteSpace(request.PhoneNumber));
+            RuleFor(request => request.FirstName)
+                .MinimumLength(3).WithMessage("First name must be at least 3 characters long.")
+                .MaximumLength(15).WithMessage("First name cannot exceed 15 characters.")
+                .When(request => !string.IsNullOrWhiteSpace(request.FirstName));
+            RuleFor(request => request.LastName)
+                .MinimumLength(3).WithMessage("Last name must be at least 3 characters long.")
+                .MaximumLength(15).WithMessage("Last name cannot exceed 15 characters.")
+                .When(request => !string.IsNullOrWhiteSpace(request.LastName));
+            RuleFor(request => request.Dni)
+                .Length(8).WithMessage("DNI must be exactly 8 characters long.")
+                .When(request => !string.IsNullOrWhiteSpace(request.Dni));
+            RuleFor(request => request.Email)
+                .EmailAddress().WithMessage("The email address format is not valid.")
+                .When(request => !string.IsNullOrWhiteSpace(request.Email));
+            RuleFor(request => request.PhoneNumber)
+                .MinimumLength(9).WithMessage("Phone number must be at least 9 characters long.")
+                .MaximumLength(11).WithMessage("Phone number cannot exceed 11 characters.")
+                .When(request => !string.IsNullOrWhiteSpace(request.PhoneNumber));
         }
     }
 }
