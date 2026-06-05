384bf790a1a8c4a9c333d3a29a2d6bbc130ddc98
AldanaIramaz <aldanairamaz@hotmail.com>
2026-06-04 23:10:00 -0300

    add veterinarianMapper and dto veterinarianRequest and veterinarianResponse

---
+++ src/VetCareBackend.Application/Mapper/VeterinarianMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VetCareBackend.Application.dtos.Requests;
+using VetCareBackend.Domain.Entities;
+using VetCareBackend.Application.dtos.Responses;
+using VetCareBackend.Domain.Enums;
+
+namespace VetCareBackend.Application.Mapper
+{
+    public static class VeterinarianMapper
+    {
+        public static VeterinarianResponse ToVeterinarianResponse(this Veterinarian veterinarian)
+        {
+            return new VeterinarianResponse
+            {
+                
+                FirstName = veterinarian.FirstName,
+                LastName = veterinarian.LastName,
+                Enrollment = veterinarian.Enrollment,
+                Speciality = veterinarian.Speciality,
+            };
+        }
+        public static Veterinarian ToVeterinarian(this VeterinarianRequest request)
+        {
+            return new Veterinarian
+            {
+                Enrollment = request.Enrollment,
+                FirstName = request.FirstName,
+                LastName= request.LastName,
+                Role = Role.Veterinarian,
+                Speciality= request.Speciality,
+            };
+        }
+
+
+
+
+
+
+    }
+
+}
---
+++ src/VetCareBackend.Application/dtos/Requests/VeterinarianRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VetCareBackend.Domain.Enums;
+
+namespace VetCareBackend.Application.dtos.Requests
+{
+    public class VeterinarianRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Enrollment { get; set; } = string.Empty;
+        public Speciality Speciality { get; set; }
+    }
+}
