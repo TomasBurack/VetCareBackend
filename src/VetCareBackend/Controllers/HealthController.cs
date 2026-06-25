aacac1f3b731eb010f8ec736afec878be0bb10c2
Tomas <tomasburack@gmail.com>
2026-06-25 00:13:02 -0300

    added health controller for verify the status of the api

---
+++ src/VetCareBackend/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VetCareBackend.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("VetCare API is running!");
+        }
+    }
+}
