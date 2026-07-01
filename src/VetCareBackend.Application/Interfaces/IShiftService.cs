de4ec595e99724ed0c65d4b00eb385c81bb6756f
AldanaIramaz <aldanairamaz@hotmail.com>
2026-06-30 23:55:28 -0300

    Add GetAllAdmin, GetAllClient, GetAllVeterinarian. Remove generic GetAll method. Update IShiftService with new methods and DateTimeOffset parameter

--- src/VetCareBackend.Application/Interfaces/IShiftService.cs
+++ src/VetCareBackend.Application/Interfaces/IShiftService.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using VetCareBackend.Application.dtos.Requests;
 using VetCareBackend.Application.dtos.Responses;
+using VetCareBackend.Domain.Enums;
 
 namespace VetCareBackend.Application.Interfaces
 {
     public interface IShiftService
     {
-        Task<List<ShiftResponse>> GetAll();
+        Task<List<ShiftResponse>> GetAllAdmin(DateTimeOffset? date, Status? status, string? enrollment);
+        Task<List<ShiftResponse>> GetAllVeterinarian(string sub);
+        Task<List<ShiftResponse>> GetAllClient(string sub);
         Task<ShiftResponse> Create(ShiftRequest shiftReq);
         Task UpdateStatus(Guid id, ShiftStatusRequest request);
         Task Delete(Guid id);
