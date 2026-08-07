fb99f361281ed36e3506e2f8f237036146852247
Tomas <tomasburack@gmail.com>
2026-08-07 00:51:16 -0300

    changed the max age validation of pet by type

--- src/VetCareBackend.Application/Validations/PetRequestValidations.cs
+++ src/VetCareBackend.Application/Validations/PetRequestValidations.cs
@@ -16,8 +16,19 @@ namespace VetCareBackend.Application.Validations
                 .MaximumLength(20).WithMessage("El nombre de la mascota no puede superar los 20 caracteres.")
                 .When(request => !string.IsNullOrWhiteSpace(request.Name));
             RuleFor(pr => pr.Age).NotEmpty().WithMessage("La edad de la mascota es obligatoria.")
-                .GreaterThanOrEqualTo(0).WithMessage("La edad de la mascota debe ser positiva.")
-                .LessThanOrEqualTo(100).WithMessage("La edad de la mascota no puede superar los 100 años.");
+                .GreaterThanOrEqualTo(0).WithMessage("La edad de la mascota debe ser positiva.");
+            RuleFor(pr => pr.Age)
+                .LessThanOrEqualTo(30).WithMessage("La edad de un perro no puede superar los 30 años.")
+                .When(pr => pr.typePet == TypePet.Canine);
+            RuleFor(pr => pr.Age)
+                .LessThanOrEqualTo(35).WithMessage("La edad de un gato no puede superar los 35 años.")
+                .When(pr => pr.typePet == TypePet.Feline);
+            RuleFor(pr => pr.Age)
+                .LessThanOrEqualTo(100).WithMessage("La edad de un ave no puede superar los 100 años.")
+                .When(pr => pr.typePet == TypePet.Avian);
+            RuleFor(pr => pr.Age)
+                .LessThanOrEqualTo(100).WithMessage("La edad de un reptil no puede superar los 100 años.")
+                .When(pr => pr.typePet == TypePet.Reptile);
             RuleFor(pr => pr.typePet)
                 .IsInEnum<PetRequest, TypePet>()
                 .WithMessage("El tipo de mascota no es válido. Por favor, seleccioná una opción válida de la lista.");
